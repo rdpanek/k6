f0c298677623623e7bfc6fd830f406fea8c76b91
Radim Daniel Panek <rdpanek@gmail.com>
2021-04-05 10:07:53 +0200

    cleaning temp tests

--- testCases/k6io.js
+++
@@ -1,9 +0,0 @@
-// docker run --name k6 -i --rm loadimpact/k6 run - < testCases/k6io.js --vus 10 --duration 30s
-
-import http from 'k6/http';
-import { sleep } from 'k6';
-
-export default function () {
-  http.get('http://test.k6.io');
-  sleep(1);
-}
