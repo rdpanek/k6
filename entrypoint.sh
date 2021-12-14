15887ebbdb9825cd68ccc6a47b6e31157ac163fd
daniel.panek <daniel.panek@rdpanek-m1.localdomain>
2021-12-14 13:05:57 +0100

    added: --out=statsd

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,4 @@ ls -lah $K6_HOME
 ( cd $K6_HOME && git fetch && git checkout $GIT_REVISION )
 
 # start k6
-k6 run $TEST_PLAN_NAME --no-usage-report
+k6 run $TEST_PLAN_NAME --out=statsd --no-usage-report
