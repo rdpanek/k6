0ae82b6eb73e779c15f5e9a6c07549d1a93cc1c9
YPR <ypr@ypr.local>
2023-09-11 00:19:14 +0200

    demos/02-books/03-advanced.js: added

---
+++ demos/02-books/03-advanced.js
@@ -0,0 +1,125 @@
+/**
+ * Advanced test for books API
+ * - books/server.js must be running before running this test
+ * - go to books folder
+ * - install dependencies: npm install
+ * - run server: node server.js
+ * 
+ * How to run only default function
+ * - k6 run -i 1 demos/02-books/03-advanced.js --env TAG="production" --env DOMAIN="http://localhost:3000"
+ * 
+ * How to run with scenarios
+ * - k6 run demos/02-books/03-advanced.js --env TAG="production" --env DOMAIN="http://localhost:3000"
+ * 
+ * How to check database
+ * - sqlite3 books/database.db "SELECT * FROM books;"
+ */
+
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+// fragments
+import { classicBooks, ebooks } from './fragments/books.js'
+
+let response
+let config = {
+  domain: __ENV.DOMAIN,
+  traceId: '1234'
+}
+
+export let options = {
+  tags: {
+    source: __ENV.TAG,
+    tc: '1234'
+  },
+  scenarios: {
+    classicBooksScenario: {
+      executor: 'ramping-vus',
+      startVUs: 1,
+      stages: [
+        { duration: '1m', target: 5 },
+        { duration: '1m', target: 10 }
+      ],
+      gracefulRampDown: '0s',
+      tags: { scenario: 'classicBooks' },
+      exec: 'classicBooksScenario',
+    },
+    ebookScenario: {
+      executor: 'ramping-vus',
+      startVUs: 5,
+      stages: [
+        { duration: '1m', target: 10 },
+        { duration: '1m', target: 20 }
+      ],
+      gracefulRampDown: '0s',
+      tags: { scenario: 'ebook' },
+      exec: 'ebookScenario',
+    }
+  },
+  thresholds: { 
+    http_req_failed: ['rate<10'],
+    http_req_duration: ['p(90)<3000'], // 90% of requests should be below 400ms
+    'group_duration{group:::Check classic books api}': ['p(95)<1000'],
+    'group_duration{group:::Check ebooks api}': ['p(95)<1000'],
+  }
+};
+
+export function setup() {
+  // Inicializace databáze pomocí API
+  response = http.post(config.domain+'/book', JSON.stringify({ 
+    type: 'běžná kniha', 
+    title: 'Testovací Běžná Kniha', 
+    author: 'Autor 1' 
+  }), { 
+    headers: { 
+      'Content-Type': 'application/json' 
+    } 
+  });
+  check(response, {
+    'Create book status code is 201': (r) => r.status === 201
+  });
+
+  response = http.post(config.domain+'/book', JSON.stringify({
+    type: 'e-kniha', 
+    title: 'Testovací E-Kniha', 
+    author: 'Autor 2' 
+  }), { 
+    headers: { 
+      'Content-Type': 'application/json' 
+    }
+  });
+  check(response, {
+    'Create ebook status code is 201': (r) => r.status === 201
+  });
+}
+
+export default function (data) {
+  classicBooksScenario(data);
+  ebookScenario(data);
+}
+
+export function classicBooksScenario(data) {
+  classicBooks(config, data);
+
+  // Random sleep to simulate user think time (1-5 seconds)
+  sleep(Math.random() * 4 + 1);
+}
+
+export function ebookScenario(data) {
+  ebooks(config, data);
+
+  // Random sleep to simulate user think time (1-5 seconds)
+  sleep(Math.random() * 4 + 1);
+}
+
+export function teardown() {
+    response = http.del(config.domain+'/book/2');
+    check(response, {
+      'Delete ebook status code is 200': (r) => r.status === 200
+    });
+
+    response = http.del(config.domain+'/book/1');
+    check(response, {
+      'Delete book status code is 200': (r) => r.status === 200
+    });
+}
---
+++ demos/02-books/fragments/books.js
@@ -0,0 +1,85 @@
+import { check, group, fail } from 'k6'
+import { Rate, Trend } from 'k6/metrics'
+import { Httpx } from 'https://jslib.k6.io/httpx/0.0.3/index.js'
+
+const openAppCheck = new Rate('openApp_check')
+const openAppTime = new Trend('openApp_time')
+let response
+
+const classicBooks = function(config = fail(`login: missing config.`), data) {
+  const { traceId, domain } = config
+  const session = new Httpx({
+    baseURL: `${domain}`,
+    headers: {
+      'x-b3-traceid': traceId,
+      //'Content-Encoding': 'gzip'
+    },
+    timeout: 20000,
+  })
+
+  group('Check classic books api', function () {
+    response = session.get('/books/běžná kniha');
+    check(response, {
+      'Get books status code is 200': (r) => r.status === 200
+    });
+    check(response, {
+      'Get books contains name of classic book': (res) => res.body.includes(`běžná kniha`)
+    });
+
+    response = session.put('/book/1', JSON.stringify({ 
+      type: 'běžná kniha', 
+      title: 'Upravená Běžná Kniha', 
+      author: 'Upravený Autor 1' 
+    }), { 
+      headers: { 
+        'Content-Type': 'application/json' 
+      } 
+    });
+    check(response, {
+      'Update book status code is 200': (r) => r.status === 200
+    });
+  })
+}
+
+
+const ebooks = function(config = fail(`login: missing config.`), data) {
+  const { traceId, domain } = config
+  const session = new Httpx({
+    baseURL: `${domain}`,
+    headers: {
+      'x-b3-traceid': traceId,
+      'Content-Encoding': 'gzip'
+    },
+    timeout: 20000,
+  })
+
+  group('Check ebooks api', function () {
+    response = session.get('/books/e-kniha');
+    check(response, {
+      'Get ebook status code is 200': (r) => r.status === 200
+    });
+    check(response, {
+      'Get books contains name of ebook': (res) => res.body.includes(`e-kniha`)
+    });
+
+    response = session.put('/book/2', JSON.stringify({ 
+      type: 'e-kniha', 
+      title: 'Upravená E-Kniha',
+      author: 'Upravený Autor 2'
+    }), { 
+      headers: { 
+        //'Content-Type': 'application/json' 
+      } });
+    check(response, {
+      'Update ebook status code is 200': (r) => r.status === 200
+    });
+  })
+}
+
+
+
+
+module.exports = {
+  classicBooks,
+  ebooks
+}
