9215a70820bab1370d2d84d19d745fa70c9449d4
Radim Daniel Pánek <rdpanek@gmail.com>
2023-10-22 21:56:45 +0000

    metrics-rate: added

---
+++ demos/features/metrics-rate-thresholds.js
@@ -0,0 +1,31 @@
+/**
+ * Custom Summary
+ * https://k6.io/docs/javascript-api/k6-metrics/counter/
+ * 
+ * How to run
+ * k6 run demos/features/metrics-rate-thresholds.js
+ */
+import { Rate } from 'k6/metrics';
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+const errorRate = new Rate('errorRate');
+
+export const options = {
+  vus: 1,
+  duration: '1m',
+  thresholds: {
+    errorRate: [
+      // more than 10% of errors will abort the test
+      { threshold: 'rate < 0.1', abortOnFail: true, delayAbortEval: '1m' },
+    ],
+  },
+};
+
+export default function () {
+  const resp = http.get('https://test-api.k6.io/public/crocodiles/1/');
+
+  errorRate.add(resp.status >= 400);
+
+  sleep(1);
+}
---
+++ demos/features/metrics-rate.js
@@ -0,0 +1,17 @@
+/**
+ * Custom Summary
+ * https://k6.io/docs/javascript-api/k6-metrics/counter/
+ * 
+ * How to run
+ * k6 run demos/features/metrics-rate.js
+ */
+import { Rate } from 'k6/metrics';
+
+const myRate = new Rate('my_rate');
+
+export default function () {
+  myRate.add(true);
+  myRate.add(false);
+  myRate.add(1);
+  myRate.add(0, { tag1: 'value', tag2: 'value2' });
+}
