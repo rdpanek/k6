85d64a065f3ac792a8fef72a34bd8cf0b446a12f
Radim Daniel Pánek <rdpanek@gmail.com>
2023-10-24 08:12:07 +0000

    demos/features/tags-and-groups.js: upgrade

--- demos/features/tags-and-groups.js
+++ demos/features/tags-and-groups.js
@@ -5,15 +5,32 @@
  * How to run
  * k6 run demos/features/tags-and-groups.js
  */
+
+// Modul execution poskytuje přístup k informacím o běhu testu, jako jsou například časové údaje, výsledky testů a další.
+import exec from 'k6/execution';
+
 import http from 'k6/http';
 import { Trend } from 'k6/metrics';
-import { check } from 'k6';
+import { check, group } from 'k6';
 
 const myTrend = new Trend('my_trend');
 
+export const options = {
+  tags: { // available acros all metrics
+    type: 'smoke',
+    tc: '3487'
+  },
+  thresholds: {
+    'http_reqs{containerGroup:main}': ['count==3'], // for code defined tags
+    'http_req_duration{containerGroup:main}': ['max<1000'], // for code defined tags
+  },
+};
+
+let res
+
 export default function () {
   // Add tag to request metric data
-  const res = http.get('https://httpbin.test.k6.io/', {
+  res = http.get('https://httpbin.test.k6.io/', {
     tags: {
       test: "abc",
     },
@@ -24,4 +41,18 @@ export default function () {
 
   // Add tag to custom metric
   myTrend.add(res.timings.connecting, { test: "abc" });
+
+  // Code defined tags
+  exec.vu.metrics.tags.containerGroup = 'main';
+  group('main', function () {
+    res = http.get('https://test.k6.io');
+    check(res, { 'status is 200': (r) => r.status === 200 });
+    group('sub', function () {
+      res = http.get('https://httpbin.test.k6.io/anything');
+      check(res, { 'status is 200': (r) => r.status === 200 });
+    });
+    res = http.get('https://test-api.k6.io');
+    check(res, { 'status is 200': (r) => r.status === 200 });
+  });
+  delete exec.vu.metrics.tags.containerGroup;
 }
