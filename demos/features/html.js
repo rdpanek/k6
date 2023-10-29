b37947872bdda251c7c4257ca0483f0b47fe7102
YPR <ypr@ypr.localdomain>
2023-10-29 22:32:59 +0100

    demos/features/html.js: updated

--- demos/features/html.js
+++ demos/features/html.js
@@ -7,6 +7,7 @@
  */
 import { parseHTML } from 'k6/html';
 import http from 'k6/http';
+import { sleep } from 'k6';
 
 export default function () {
   const res = http.get('https://k6.io');
@@ -15,5 +16,26 @@ export default function () {
   const langAttr = doc.find('html').attr('lang');
   console.log(`Page title: ${pageTitle}`);
   console.log(`Page attributes: ${langAttr}`);
+
+  const content = `
+  <dl>
+    <dt id="term-1">Value term 1</dt>
+    <dt id="term-2">Value term 2</dt>
+  </dl>
+    `;
+  const sel = parseHTML(content).find('dl').children(); // pole elementů
+
+  const el1 = sel.get(0);
+  const el2 = sel.get(1);
+
+  console.log(el1.nodeName()); // dt
+  console.log(el1.id()); // term-1
+  console.log(el1.textContent()); // Value term 1
+
+  console.log(el2.nodeName()); // dt
+  console.log(el2.id()); // term-2
+  console.log(el2.textContent()); // Value term 2
+
+  sleep(1);
 }
 
