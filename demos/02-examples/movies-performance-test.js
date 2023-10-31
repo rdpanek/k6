cc7fc07669383a006cb79fc3d337579318635213
YPR <ypr@ypr.localdomain>
2023-10-31 11:27:56 +0100

    homework template: Marvel Movies Home Work

---
+++ demos/02-examples/movies-performance-test.js
@@ -0,0 +1,51 @@
+/**
+ * Marvel Movies Home Work
+ * 
+ * How to run
+ * k6 run demos/02-examples/movies-performance-test.js 
+ * 
+ */
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+  let documents = [
+    {
+      title: 'Iron Man',
+      release_date: '2008-05-02',
+      actors: ['Robert Downey Jr.', 'Gwyneth Paltrow', 'Terrence Howard'],
+      description: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
+      rating: 7.9,
+    },
+    {
+      title: 'The Incredible Hulk',
+      release_date: '2008-06-13',
+      actors: ['Edward Norton', 'Liv Tyler', 'Tim Roth'],
+      description: 'Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into whenever he loses his temper.',
+      rating: 6.7,
+    },
+    {
+      title: 'Thor',
+      release_date: '2011-05-06',
+      actors: ['Chris Hemsworth', 'Anthony Hopkins', 'Natalie Portman'],
+      description: 'The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.',
+      rating: 7.0,
+    },
+    {
+      title: 'Captain America: The First Avenger',
+      release_date: '2011-07-22',
+      actors: ['Chris Evans', 'Hugo Weaving', 'Samuel L. Jackson'],
+      description: 'Steve Rogers, a rejected military soldier, transforms into Captain America after taking a dose of a "Super-Soldier serum". But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization.',
+      rating: 6.9,
+    },
+  ];
+
+  for (let document of documents) {
+    let res = http.post('http://localhost:9200/movies/_doc', JSON.stringify(document), { headers: { 'Content-Type': 'application/json' } });
+
+    // Kontrola, zda byl požadavek úspěšný
+    check(res, {
+      'status was 201': (r) => r.status === 201,
+    });
+  }
+}
