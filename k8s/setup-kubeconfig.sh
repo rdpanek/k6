e7275ac7d41d8398b719029ad7cec63d8eb580ce
ypr <ypr@ypr--Air.localdomain>
2026-03-31 15:24:46 +0200

    Devcontainer: update

--- k8s/setup-kubeconfig.sh
+++ k8s/setup-kubeconfig.sh
@@ -2,5 +2,5 @@
 nano /workspaces/k6/kubeconfig.yaml
 export KUBECONFIG=/workspaces/k6/kubeconfig.yaml
 echo 'export KUBECONFIG=/workspaces/k6/kubeconfig.yaml' >> ~/.bashrc
-echo "Done. Checking connection..."
-kubectl get nodes
+echo 'Done. Now run: export KUBECONFIG=/workspaces/k6/kubeconfig.yaml'
+echo 'Or open a new terminal.'
